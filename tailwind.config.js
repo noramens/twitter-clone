57c12f3b4de51f24517dbefc035e65efe38b3c1d
Nora Mensah <noramens@mpharma.com>
2023-07-24 23:19:46 +0000

    Add sidebar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,21 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
     },
+    extend: {
+      colors: {
+        primary: 'rgb(29, 155, 240)'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
